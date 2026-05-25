ce46a8198b66ab300ea508679d6c27fcf9b790a5
Joseph Lodico <josephl2020@hotmail.com>
2026-05-25 17:15:07 -0400

    - Added a bunch of Getters and Setters inside Profile.cs - Moved DisplayProfile to the main Program

---
+++ ProfileManager/Classes/Profile.cs
@@ -0,0 +1,29 @@
+﻿/*                              USER PROFILE SYSTEM                           */
+namespace ProfileManager.Classes
+{
+    public class Profile
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+
+        public int Age { get; set; }
+        public int Gender { get; set; }
+        public int Country { get; set; }
+        public int Province { get; set; }
+        public int DateOfBirth { get; set; }
+
+        public string Hobby { get; set; }  
+        public string FavoriteGame { get; set; }
+        public string FavoriteAnime { get; set; }
+        public string Pet { get; set; }
+
+
+        public double Weight { get; set; }
+        public double Height { get; set; }
+
+       
+    }
+}
--- ProfileManager/UserProfileSystem.cs
+++ ProfileManager/UserProfileSystem.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using ProfileManager.Classes;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Net;
+using System.Reflection;
 
 
 /*                              USER PROFILE SYSTEM                           */
@@ -9,7 +13,31 @@ namespace ProfileManager
     {
         static void Main(string[] args)
         {
+            var profile = new Profile();
+        }
+        public void DisplayProfile()
+        {
+            var profile = new Profile();
+            Console.WriteLine("===== PROFILE =====");
 
+            Console.WriteLine($"Name: {profile.FirstName} {profile.LastName}");
+            Console.WriteLine($"Email: {profile.Email}");
+            Console.WriteLine($"Phone Number: {profile.PhoneNumber}");
+            Console.WriteLine($"Address: {profile.Address}");
+
+            Console.WriteLine($"Age: {profile.Age}");
+            Console.WriteLine($"Gender: {profile.Gender}");
+            Console.WriteLine($"Country: {profile.Country}");
+            Console.WriteLine($"Province: {profile.Province}");
+            Console.WriteLine($"Date of Birth: {profile.DateOfBirth}");
+
+            Console.WriteLine($"Hobby: {profile.Hobby}");
+            Console.WriteLine($"Favorite Game: {profile.FavoriteGame}");
+            Console.WriteLine($"Favorite Anime: {profile.FavoriteAnime}");
+            Console.WriteLine($"Pet: {profile.Pet}");
+
+            Console.WriteLine($"Weight: {profile.Weight}");
+            Console.WriteLine($"Height: {profile.Height}");
         }
     }
 }
