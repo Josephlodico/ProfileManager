01ccaf1726d8382e685b872000cedada1a8a661c
Joseph Lodico <josephl2020@hotmail.com>
2026-05-25 16:47:18 -0400

    Setting up the Main Program.

---
+++ ProfileManager/UserProfileSystem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+/*                              USER PROFILE SYSTEM                           */
+namespace ProfileManager
+{
+    class userProfileSystem
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
